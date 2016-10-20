04bcd822e07e9d6c0e15b749edac12736e328e93
kerry.qiu <kerry.qiu@ef.com>
2016-10-20 15:22:22 +0800

    add files for jenkins running

--- tools/clean_webdriver.py
+++ tools/clean_webdriver.py
@@ -4,7 +4,7 @@ this module used to kill all the webdrivers & browsers after the jenkins job exe
 import os
 
 if "__main__" == __name__:
-    os.system("taskkill /f /im chrome.exe")
+    # os.system("taskkill /f /im chrome.exe")
     os.system("taskkill /f /im chromedriver.exe")
     os.system("taskkill /f /im firefox.exe")
     os.system("taskkill /f /im iexplore.exe")
